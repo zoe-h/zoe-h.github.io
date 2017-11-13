f0a5e379ac11cea4894e2e80cfa975bb32ca9e7c
zoe-h <30714392+zoe-h@users.noreply.github.com>
2017-11-13 13:29:47 +1100

    changes

--- zoehu.co_test/assets/js/main.js
+++ zoehu.co_test/assets/js/main.js
@@ -4,77 +4,102 @@ $(document).ready(function() {
 
 
  //image hover
-if(screen.width < 768) {
+if(screen.width > 768) {
+
+  $('#whittakers img').mouseenter(function() {
+    $('img').addClass('hide_img');
+    $('#whittakers img').removeClass('hide_img');
+    $('body').addClass('whittakers_bg');
+  });
+
+  $('#whittakers img').mouseleave(function() {
+    $('img').removeClass('hide_img');
+    $('body').removeClass('whittakers_bg');
+  });
+
+   $('#lynch img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#lynch img').removeClass('hide_img');
+     $('body').addClass('lynch_bg');
+   });
+
+   $('#lynch img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('lynch_bg');
+   });
+
+   $('#wander img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#wander img').removeClass('hide_img');
+     $('body').addClass('wander_bg');
+   });
+
+   $('#wander img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('wander_bg');
+   });
+
+   $('#graphicdesign img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#graphicdesign img').removeClass('hide_img');
+     $('body').addClass('graphicdesign_bg');
+   });
+
+   $('#graphicdesign img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('graphicdesign_bg');
+   });
+
+
+//    $('#sock img').mouseenter(function() {
+//      $('img').addClass('hide_img');
+//      $('#sock img').removeClass('hide_img');
+//      $('body').addClass('sock_bg');
+//    });
+
+//    $('#sock img').mouseleave(function() {
+//      $('img').removeClass('hide_img');
+//        $('body').removeClass('sock_bg');
+//    });
+
+   $('#mirror img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#mirror img').removeClass('hide_img');
+     $('body').addClass('mirror_bg');
+   });
+
+   $('#mirror img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('mirror_bg');
+   });
+
+   $('#forum img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#forum img').removeClass('hide_img');
+     $('body').addClass('forum_bg');
+   });
+
+   $('#forum img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('forum_bg');
+   });
+
+   $('#spring img').mouseenter(function() {
+     $('img').addClass('hide_img');
+     $('#spring img').removeClass('hide_img');
+     $('body').addClass('spring_bg');
+   });
+
+   $('#spring img').mouseleave(function() {
+     $('img').removeClass('hide_img');
+     $('body').removeClass('spring_bg');
+   });
+
   return;
 } else {
-     $('#whittakers img').mouseenter(function() {
-       $('img').addClass('hide_img');
-       $('#whittakers img').removeClass('hide_img');
-       $('body').addClass('whittakers_bg');
-     });
-
-     $('#whittakers img').mouseleave(function() {
-       $('img').removeClass('hide_img');
-       $('body').removeClass('whittakers_bg');
-     });
-
-      $('#lynch img').mouseenter(function() {
-        $('img').addClass('hide_img');
-        $('#lynch img').removeClass('hide_img');
-        $('body').addClass('lynch_bg');
-      });
-
-      $('#lynch img').mouseleave(function() {
-        $('img').removeClass('hide_img');
-        $('body').removeClass('lynch_bg');
-      });
-
-      $('#wander img').mouseenter(function() {
-        $('img').addClass('hide_img');
-        $('#wander img').removeClass('hide_img');
-        $('body').addClass('wander_bg');
-      });
-
-      $('#wander img').mouseleave(function() {
-        $('img').removeClass('hide_img');
-        $('body').removeClass('wander_bg');
-      });
-
-      $('#graphicdesign img').mouseenter(function() {
-        $('img').addClass('hide_img');
-        $('#graphicdesign img').removeClass('hide_img');
-        $('body').addClass('graphicdesign_bg');
-      });
-
-      $('#graphicdesign img').mouseleave(function() {
-        $('img').removeClass('hide_img');
-        $('body').removeClass('graphicdesign_bg');
-      });
 
 
-      $('#sock img').mouseenter(function() {
-        $('img').addClass('hide_img');
-        $('#sock img').removeClass('hide_img');
-        $('body').addClass('sock_bg');
-      });
-
-      $('#sock img').mouseleave(function() {
-        $('img').removeClass('hide_img');
-        $('body').removeClass('sock_bg');
-      });
-
-      $('#mirror img').mouseenter(function() {
-        $('img').addClass('hide_img');
-        $('#mirror img').removeClass('hide_img');
-        $('body').addClass('mirror_bg');
-      });
-
-      $('#mirror img').mouseleave(function() {
-        $('img').removeClass('hide_img');
-        $('body').removeClass('mirror_bg');
-      });
-
-}
+};
 
   // about / info
 
@@ -123,29 +148,7 @@ $('.about').click(function() {
 });
 
 
-// time
 
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
-
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  // add a zero in front of numbers<10
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-  t = setTimeout(function() {
-    startTime()
-  }, 500);
-}
-startTime();
 
 
 
